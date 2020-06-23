820434f56033429fcf14b44cf3cee3a7b20a172c
Angel García-Campos <xxwildfangxx@gmail.com>
2020-06-23 15:28:17 -0500

    update to 21 June

--- app.R
+++ app.R
@@ -22,12 +22,12 @@ ui <- fluidPage(
         ),
         sidebarPanel(
             fluidRow(h4("Autor: ", a(href="https://angelcampos.github.io/", "AngelCampos"))),
-        fluidRow(h4("Datos abiertos: ", a(href="https://www.apple.com/covid19/mobility", "Apple Mobility Trends Reports.")), "Última actualización de datos: 15 de Junio, 2020"),
+        fluidRow(h4("Datos abiertos: ", a(href="https://www.apple.com/covid19/mobility", "Apple Mobility Trends Reports.")), "Última actualización de datos: 21 de Junio, 2020"),
             fluidRow(align = "center", HTML("<style>.bmc-button img{height: 34px !important;width: 35px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button{padding: 7px 15px 7px 10px !important;line-height: 35px !important;height:51px !important;text-decoration: none !important;display:inline-flex !important;color:#ffffff !important;background-color:#5F7FFF !important;border-radius: 5px !important;border: 1px solid transparent !important;padding: 7px 15px 7px 10px !important;font-size: 28px !important;letter-spacing:0.6px !important;box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;margin: 0 auto !important;font-family:'Cookie', cursive !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;}.bmc-button:hover, .bmc-button:active, .bmc-button:focus {-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;text-decoration: none !important;box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;opacity: 0.85 !important;color:#ffffff !important;}</style><link href='https://fonts.googleapis.com/css?family=Cookie' rel='stylesheet'><a class='bmc-button' target='_blank' href='https://www.buymeacoffee.com/AngelCampos'><img src='https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg' alt='Dona!'><span style='margin-left:5px;font-size:28px !important;'>Dona!</span></a>")),
             hr(),
-            fluidRow(p("Esta aplicación grafica el porcentaje de movilidad diario por estado y para algunas de las ciudades de México.",
-                       "El punto de referencia (0%) es la movilidad calculada para el día lunes, 13 de enero del 2020."),
-                     p("Para graficar distintos valores, usa el menú inferior para seleccionar los",
+            fluidRow(p("Esta aplicación grafica el porcentaje de movilidad diario por estado y para algunas ciudades de México.",
+                       "El punto de referencia (0%) es la movilidad calculada para el día lunes, 13 de enero del 2020, en cada región"),
+                     p("Para graficar los valores de distintas regiones, usa el menú inferior para seleccionar los",
                        strong("estados o ciudades,"), "cambiar el", strong("rango de días,"),
                        "y el tipo de", strong("transporte."))),
                      hr(),
@@ -68,7 +68,6 @@ server <- function(input, output){
         gg
     })
 }
-
 
 # Run the application
 shinyApp(ui = ui, server = server)
